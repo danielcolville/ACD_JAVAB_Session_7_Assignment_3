3eab0c1acbe6e0acb6bba6bfa1474d08986c2311
danielcolville <53836490+danielcolville@users.noreply.github.com>
2019-08-13 16:28:05 -0400

    Add files via upload

---
+++ StringBufferSize.java
@@ -0,0 +1,13 @@
+package session7;
+
+public class StringBufferSize {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuffer str=new StringBuffer("This is");
+		
+		str.append("a String");
+		str.append("Of characters");
+	}
+
+}

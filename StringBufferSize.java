5b9efebbe95b71669803492a1df1155ba7a5d0ef
danielcolville <53836490+danielcolville@users.noreply.github.com>
2019-08-14 15:50:25 -0400

    Update StringBufferSize.java

--- StringBufferSize.java
+++ StringBufferSize.java
@@ -3,7 +3,6 @@ package session7;
 public class StringBufferSize {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		StringBuffer str=new StringBuffer("This is");
 		System.out.println(str);
 		System.out.println(str.capacity());

15b46aa108702272045c6b1d86c55b4dfccd715a
danielcolville <53836490+danielcolville@users.noreply.github.com>
2019-08-14 15:50:09 -0400

    Update StringBufferSize.java

--- StringBufferSize.java
+++ StringBufferSize.java
@@ -5,9 +5,17 @@ public class StringBufferSize {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		StringBuffer str=new StringBuffer("This is");
-		
-		str.append("a String");
-		str.append("Of characters");
+		System.out.println(str);
+		System.out.println(str.capacity());
+		str.append(" a String");
+		System.out.println(str);
+		System.out.println(str.capacity());
+		str.append(" Of characters");
+		System.out.println(str);
+		System.out.println(str.capacity());
+		str.append(" and lots and lots of more characters to test how the buffer grows");
+		System.out.println(str);
+		System.out.println(str.capacity());
 	}
 
 }
